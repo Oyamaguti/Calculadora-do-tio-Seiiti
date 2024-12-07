222b53e7feeacf0e953cbf6d531c4352ee396a1d
Oyamaguti <vichavesoyamaguti@gmail.com>
2024-12-07 20:44:28 -0300

    Sistema de apagar um número/operação por vez

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function inserir (numero) {
     let num = document.querySelector('.visor').innerHTML
     document.querySelector('.visor').innerHTML = num + numero
 }
+
+function apagar() {
+    let resultado = document.querySelector('.visor').innerHTML
+    document.querySelector('.visor').innerHTML = resultado.substring(0, resultado.length -1)
+}
