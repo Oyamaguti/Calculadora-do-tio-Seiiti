a458ca2b7d5b8d978ef6253e6f36d073365f430e
Oyamaguti <vichavesoyamaguti@gmail.com>
2024-12-07 20:58:58 -0300

    adição da função calcular (sinal de igual)

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function inserir (numero) {
-    let num = document.querySelector('.visor').innerHTML
-    document.querySelector('.visor').innerHTML = num + numero
+    let num = document.querySelector('.conta').innerHTML
+    document.querySelector('.conta').innerHTML = num + numero
 }
 
 function apagar() {
-    let resultado = document.querySelector('.visor').innerHTML
-    document.querySelector('.visor').innerHTML = resultado.substring(0, resultado.length -1)
+    let conta = document.querySelector('.conta').innerHTML
+    document.querySelector('.conta').innerHTML = conta.substring(0, conta.length -1)
+    if (conta.length - 1) {
+        let resultado = document.querySelector('.visor').innerHTML
+        document.querySelector('.visor').innerHTML = ''
+    }
+
+}
+
+function calculo() {
+    let resultadocalculadora = document.querySelector('.conta').innerHTML
+    if (resultadocalculadora) {
+        document.querySelector('.visor').innerHTML = eval(resultadocalculadora)
+    }
 }
